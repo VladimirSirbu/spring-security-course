42059acb6470c31e5ec296a11951ed2dde1e463e
Vladimir <sirbuvova4@gmail.com>
2022-09-05 22:44:43 +0300

    Permission Based Authentication - Setting authorities to users

--- src/main/java/com/example/springsecuritycourse/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritycourse/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springsecuritycourse.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.springsecuritycourse.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.springsecuritycourse.security.ApplicationUserRole.*;
 
 @Configuration
@@ -28,10 +30,14 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()// by default S. Security protect API (when you want to POST, PUT, DELTE). And disabling CSRF() you cancel this protection
+                .csrf().disable() // by default S. Security protect API (when you want to POST, PUT, DELTE). And disabling CSRF() you cancel this protection
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "management/api/v1/students").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "management/api/v1/students").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "management/api/v1/students").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "management/api/v1/students").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
@@ -44,19 +50,22 @@ public class ApplicationSecurityConfig {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("annapass1234"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("lindapass1234"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("tompass1234"))
-                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
 
