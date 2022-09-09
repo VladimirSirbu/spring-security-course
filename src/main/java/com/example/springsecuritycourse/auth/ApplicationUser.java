fbdb89414828b0d4c5c41080cfc1645e6ed635bb
Vladimir <sirbuvova4@gmail.com>
2022-09-09 11:07:52 +0300

    ApplicationUser Model

---
+++ src/main/java/com/example/springsecuritycourse/auth/ApplicationUser.java
@@ -0,0 +1,64 @@
+package com.example.springsecuritycourse.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+public class ApplicationUser implements UserDetails {
+
+    private final List<? extends GrantedAuthority> authorities;
+    private final String password;
+    private final String username;
+    private final boolean isAccountNonExpired;
+    private final boolean isAccountNonLocked;
+    private final boolean isCredentialsNonExpired;
+
+    public ApplicationUser(List<? extends GrantedAuthority> authorities,
+                           String password, String username,
+                           boolean isAccountNonExpired,
+                           boolean isAccountNonLocked,
+                           boolean isCredentialsNonExpired) {
+        this.authorities = authorities;
+        this.password = password;
+        this.username = username;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
